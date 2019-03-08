fe9ea9312d96171e9cd82485d98133d4f557dcda
Ronan GRIOT <rgriot@IFREMER.FR>
2019-03-08 09:25:11 +0100

    v0.23

--- R/APIS_functions.R
+++ R/APIS_functions.R
@@ -16,7 +16,7 @@
 #' p = number of markers
 #' rownames(dam) = labels of dams
 #' marker coding = "All1/All2" example: "A/A", "A/B", "NA/NA" (for missing genotype)
-#' @param error (default: NULL) The assignment error rate accepted by the user
+#' @param error (default: 0) The assignment error rate accepted by the user
 #' @param exclusion.threshold (default: ncol(off.genotype)) Threshold for exclusion (number of mismatches allowed)
 #' @param preselect.Parent (default: FALSE) Preselection of parents. Can be FALSE, an integer or a vector of two integers (number of sires, numbers of dams)
 #' @keywords assignment APIS
@@ -29,7 +29,7 @@
 #'                dam.genotype = dam_full,
 #'                error = 0.05)
 #' @export
-APIS <- function(off.genotype, sire.genotype, dam.genotype, error = NULL, exclusion.threshold = ncol(off.genotype), preselect.Parent = F) {
+APIS <- function(off.genotype, sire.genotype, dam.genotype, error = 0, exclusion.threshold = ncol(off.genotype), preselect.Parent = F) {
 
   # Check inputs
 
@@ -100,341 +100,6 @@ APIS <- function(off.genotype, sire.genotype, dam.genotype, error = NULL, exclus
 }
 
 # ----------------------------------------------------------------------------------------------------------------
-#' Assignment function to obtain the average Mendelian transmission probabilities
-#'
-#' This function calculates the average Mendelian transmission probabilities
-#' @param offspring Offspring genotypes | Matrix (n*p) where n = number of individuals, p = number of markers
-#' rownames(offspring) = labels of offspring
-#' marker coding = "All1/All2" example: "A/A", "A/B", "NA/NA" (for missing genotype)
-#' @param sire Sire genotypes | Matrix (n*p) where n = number of individuals, p = number of markers
-#' rownames(sire) = labels of sires
-#' marker coding = "All1/All2" example: "A/A", "A/B", "NA/NA" (for missing genotype)
-#' @param dam Dam genotypes | Matrix (n*p) where n = number of individuals, p = number of markers
-#' rownames(dam) = labels of dams
-#' marker coding = "All1/All2" example: "A/A", "A/B", "NA/NA" (for missing genotype)
-#' @param thresh (default: ncol(offspring) Threshold for exclusion (number of mismatches allowed)
-#' @param preselect.Parent (default: FALSE) Preselection of parents. Can be FALSE, an integer or a vector of two integers (number of sires, numbers of dams)
-#' @keywords assignment
-#' @return intermidiate pedigree
-#' @return log file for Mendelian transmission probabilities
-#' @return log file for exclusion
-assignment <- function(offspring, sire, dam, thresh = ncol(offspring), preselect.Parent = F) {
-  # DESCRIPTION
-  # Function to calculate average Mendelian transmission probabilities
-
-  # Stop if different number of markers are provided
-  if (ncol(offspring)!=ncol(sire)&ncol(offspring)!=ncol(dam))
-    stop('Genotypes must have the same number of markers')
-
-  # Create the results matrix
-  ped.log <- as.data.frame(matrix(NA,nrow = nrow(offspring), ncol = 16))
-  colnames(ped.log) <- c('off', 'mrk_genotype','sire1', 'dam1', 'miss1', 'mendel1',
-                         'sire2', 'dam2', 'miss2', 'mendel2', 'delta_Pmendel12',
-                         'sire3', 'dam3', 'miss3', 'mendel3', 'delta_Pmendel23')
-  ped.log[,1] <- rownames(offspring)
-  ped.log[,2] <- as.numeric(apply(offspring,1, function(X) {length(X[X!='NA/NA'])}))
-
-  ped.exclu <- as.data.frame(matrix(NA, nrow = nrow(offspring), ncol = 11))
-  colnames(ped.exclu) <- c('off', 'mrk_genotype','sire1', 'dam1', 'miss1',
-                           'sire2', 'dam2', 'miss2',
-                           'sire3', 'dam3', 'miss3')
-
-  # Parameters
-  e <- 0.01 # Genotyping error of 1%
-
-  # Estimate allele frequencies
-  cat('Estimation of allele frequencies')
-  cat('\n')
-  Freq <- allFreq(offspring)
-
-  x.col <- ncol(offspring)
-
-  # Exclusion tables
-  homo_exclu <- matrix(c(0,0,1,0,
-                         0,0,1,0,
-                         1,1,2,1,
-                         0,0,1,0), nrow = 4, ncol = 4)
-  rownames(homo_exclu) <- c('AA', 'AC', 'CC', 'miss')
-  colnames(homo_exclu) <- c('AA', 'AC', 'CC', 'miss')
-
-  hetero_exclu <- matrix(c(1,0,0,1,0,1,0,
-                           0,0,0,0,0,1,0,
-                           0,0,1,0,1,1,0,
-                           1,0,0,1,0,1,0,
-                           0,0,1,0,1,1,0,
-                           1,1,1,1,1,2,1,
-                           0,0,0,0,0,1,0), nrow = 7, ncol = 7)
-  rownames(hetero_exclu) <- c('AA', 'AB', 'BB', 'AC', 'BC', 'CC', 'miss')
-  colnames(hetero_exclu) <- c('AA', 'AB', 'BB', 'AC', 'BC', 'CC', 'miss')
-
-  # Variables for parent selection
-  parent.genotype <- rbind(sire, dam)
-
-  parent.sex <- c(rep("M", times = nrow(sire)), rep("F", times = nrow(dam)))
-
-  # Assignment Process
-  cat('Assignment')
-  cat('\n')
-
-  # Set up the cluster for parallel iteration
-  cl <- makeCluster(parallel::detectCores()-1)
-  registerDoSNOW(cl)
-
-  iterations <- nrow(offspring)
-  pb <- txtProgressBar(min = 0, max = iterations, char = "><(((°> ", style = 3)
-
-  progress <- function(n) {setTxtProgressBar(pb, n)}
-
-  opts <- list(progress = progress)
-
-  # Start
-  A <- foreach(off=1:iterations, .multicombine = T,
-               .packages = c('foreach', 'doParallel', 'doSNOW'), .options.snow = opts) %dopar% { # For each offspring
-
-                 tmp <- offspring[off,, drop = F]
-
-
-                 if (preselect.Parent == F) {
-                   potential.sire <- rownames(sire)
-                   potential.dam <- rownames(dam)
-                 } else {
-                   potential.parents <- selectParents(tmp, parent.genotype = parent.genotype,
-                                                      parent.sex = parent.sex, n.Parent = preselect.Parent)
-
-                   potential.sire <- potential.parents$sire_toKeep
-                   potential.dam <- potential.parents$dam_toKeep
-                 }
-
-
-                 # Create temporary results
-                 res <- matrix(NA, nrow = (length(potential.sire)*length(potential.dam)), ncol = 4)
-                 colnames(res) <- c('sire', 'dam', 'score_exclu', 'P_mendel')
-                 res[,1] <- rep(potential.sire, each = length(potential.dam))
-                 res[,2] <- rep(potential.dam, times = length(potential.sire))
-
-                 # Lielihood tables
-                 table_like <- vector('list', x.col)
-                 off_geno <- strsplit(tmp, split = "/", fixed = T)
-                 for (i in 1:length(tmp)) {
-                   if (off_geno[[i]][1]==off_geno[[i]][2]) {
-                     fa <- Freq[i,which(colnames(Freq)==paste0('Freq_',off_geno[[i]][1]))]
-                     fa <- ifelse(test = (fa==0|is.na(fa)), yes = Freq[i,which(colnames(Freq)==paste0('Freq_NA'))], no = fa)
-                     table_like[[i]] <- matrix(c(1,0.5,e,fa,0.5,0.25,e,0.5*fa,e,e,e,e,fa,0.5*fa,e,fa*fa), nrow = 4, ncol = 4)
-                   } else {
-                     fa <- Freq[i,which(colnames(Freq)==paste0('Freq_',off_geno[[i]][1]))]
-                     fa <- ifelse(test = (fa==0|is.na(fa)), yes = Freq[i,which(colnames(Freq)==paste0('Freq_NA'))], no = fa)
-                     fb <- Freq[i,which(colnames(Freq)==paste0('Freq_',off_geno[[i]][2]))]
-                     fb <- ifelse(test = (fb==0|is.na(fb)), yes = Freq[i,which(colnames(Freq)==paste0('Freq_NA'))], no = fb)
-
-                     table_like[[i]] <- matrix(c(e,0.5,1,e,0.5,e,fb,
-                                                 0.5,0.5,0.5,0.25,0.5,e,0.5*(fa+fb),
-                                                 1,0.5,e,0.5,e,e,fa,
-                                                 e,0.25,0.5,e,0.25,e,0.5*fb,
-                                                 0.5,0.25,e,0.25,e,e,0.5*fb,
-                                                 e,e,e,e,e,e,e,
-                                                 fb,0.5*(fa+fb),fa,0.5*fb,0.5*fa,e,2*fa*fb), nrow = 7, ncol = 7)
-                   }
-                 }
-
-                 t <- foreach(n = 1:nrow(res), .combine = rbind,
-                              .multicombine = T, .packages = c('foreach', 'doParallel', 'doSNOW')) %dopar% { # For each parents pair
-
-                                #p = sire // m = dam
-                                p <- sire[which(rownames(sire)==res[n,1]),, drop = F]
-                                m <- dam[which(rownames(dam)==res[n,2]),, drop = F]
-
-                                # Keep likelihood and missmatches
-                                sc_exclu <- vector(mode = 'numeric', length = x.col)
-                                sc_like <- vector(mode = 'numeric', length = x.col)
-
-                                off_split <- strsplit(tmp, split = "/", fixed = T)
-                                p_split <- strsplit(p, split = "/", fixed = T)
-                                m_split <- strsplit(m, split = "/", fixed = T)
-
-                                mrk <- 1
-
-                                while (mrk <= x.col & sum(sc_exclu) <= thresh) { # For each marker
-                                  off_mrk <- off_split[[mrk]]
-                                  p_mrk <- p_split[[mrk]]
-                                  m_mrk <- m_split[[mrk]]
-
-                                  if (off_mrk[1]=='NA'&off_mrk[2]=='NA') { # if offspring genotype is missing
-                                    sc_exclu[mrk] <- 0
-                                    sc_like[mrk] <- 1
-                                  } else if (off_mrk[1]==off_mrk[2]&off_mrk[1]!='NA') { #If offspring is HOMOZYGOUS
-                                    # SIRE ID
-                                    if (p_mrk[1]==p_mrk[2]) { # If the sire is HOMOZYGOUS
-                                      if (p_mrk[1]=='NA') {
-                                        p_id <- 4 # sire NA/NA
-                                      }else if (p_mrk[1]==off_mrk[1]) {
-                                        p_id <- 1 # sire A/A
-                                      } else {
-                                        p_id <- 3 # sire "C/C"
-                                      }
-                                    } else { # If the sire is HETEROZYGOUS
-                                      if ((p_mrk[1]!=off_mrk[1]&p_mrk[2]!=off_mrk[1])&(p_mrk[1]!=off_mrk[2]&p_mrk[2]!=off_mrk[2])) {
-                                        p_id <- 3 # sire C/C
-                                      } else {
-                                        p_id <- 2 # sire A/C
-                                      }
-                                    }
-                                    #DAM ID
-                                    if (m_mrk[1]==m_mrk[2]) { #If the dam is HOMOZYGOUS
-                                      if (m_mrk[1]=='NA') {
-                                        m_id <- 4 # dam NA/NA
-                                      }else if (m_mrk[1]==off_mrk[1]) {
-                                        m_id <- 1 # dam A/A
-                                      } else {
-                                        m_id <- 3 # dam C/C
-                                      }
-                                    } else { # If the dam is HETEROZYGOUS
-                                      if ((m_mrk[1]!=off_mrk[1]&m_mrk[2]!=off_mrk[1])&(m_mrk[1]!=off_mrk[2]&m_mrk[2]!=off_mrk[2])) {
-                                        m_id <- 3 # dam C/C
-                                      } else {
-                                        m_id <- 2 # dam A/C
-                                      }
-                                    }
-
-                                    sc_exclu[mrk] <- homo_exclu[m_id,p_id]
-                                    sc_like[mrk] <- table_like[[mrk]][m_id, p_id]
-
-                                  } else { #If the offspring is HETEROZYGOUS
-                                    # SIRE ID
-                                    if (p_mrk[1]==p_mrk[2]) { # If the sire is HOMOZYGOUS
-                                      if (p_mrk[1]=='NA') {
-                                        p_id <- 7 # sire NA/NA
-                                      }else if (p_mrk[1]==off_mrk[1]) {
-                                        p_id <- 1 # sire A/A
-                                      } else if (p_mrk[1]==off_mrk[2]) {
-                                        p_id <- 3 # sire B/B
-                                      } else {
-                                        p_id <- 6 # sire C/C
-                                      }
-                                    } else { # If the sire is HETEROZYGOUS
-                                      if ((p_mrk[1]==off_mrk[1]|p_mrk[2]==off_mrk[1])&(p_mrk[1]==off_mrk[2]|p_mrk[2]==off_mrk[2])) {
-                                        p_id <- 2 # sire A/B
-                                      } else if ((p_mrk[1]==off_mrk[1]|p_mrk[2]==off_mrk[1])&(p_mrk[1]!=off_mrk[2]|p_mrk[2]!=off_mrk[2])){
-                                        p_id <- 4 # sire A/C
-                                      } else if ((p_mrk[1]!=off_mrk[1]|p_mrk[2]!=off_mrk[1])&(p_mrk[1]==off_mrk[2]|p_mrk[2]==off_mrk[2])) {
-                                        p_id <- 5 # sire B/C
-                                      } else {
-                                        p_id <- 6 # sire C/C
-                                      }
-                                    }
-
-                                    # DAM ID
-                                    if (m_mrk[1]==m_mrk[2]) { # If the dam is HOMOZYGOUS
-                                      if (m_mrk[1]=='NA') {
-                                        m_id <- 7 # dam NA/NA
-                                      }else if (m_mrk[1]==off_mrk[1]) {
-                                        m_id <- 1 # dam A/A
-                                      } else if (m_mrk[1]==off_mrk[2]) {
-                                        m_id <- 3 # dam B/B
-                                      } else {
-                                        m_id <- 6 # dam C/C
-                                      }
-                                    } else { # If the dam is HETEROZYGOUS
-                                      if ((m_mrk[1]==off_mrk[1]|m_mrk[2]==off_mrk[1])&(m_mrk[1]==off_mrk[2]|m_mrk[2]==off_mrk[2])) {
-                                        m_id <- 2 # dam A/B
-                                      } else if ((m_mrk[1]==off_mrk[1]|m_mrk[2]==off_mrk[1])&(m_mrk[1]!=off_mrk[2]|m_mrk[2]!=off_mrk[2])){
-                                        m_id <- 4 # dam A/C
-                                      } else if ((m_mrk[1]!=off_mrk[1]|m_mrk[2]!=off_mrk[1])&(m_mrk[1]==off_mrk[2]|m_mrk[2]==off_mrk[2])) {
-                                        m_id <- 5 # dam B/C
-                                      } else {
-                                        m_id <- 6 # dam C/C
-                                      }
-                                    }
-
-                                    # Get the score for tested marker
-                                    sc_exclu[mrk] <- hetero_exclu[m_id,p_id]
-                                    sc_like[mrk] <- table_like[[mrk]][m_id, p_id]
-                                  }
-
-                                  mrk <- mrk + 1
-
-                                }
-
-                                # Create the parallel loop output
-                                r <- c(NA,NA)
-                                r[1] <- sum(sc_exclu) # Number of missmatch
-                                r[2] <- exp(sum(log(sc_like))/ped.log[off,2]) # Mendelian transimission probability
-
-                                return(r)
-                              }
-
-                 # Working on the results
-                 res[,3:4] <- t
-                 res <- as.data.frame(res)
-                 res$sire <- as.character(res$sire)
-                 res$dam <- as.character(res$dam)
-                 res$score_exclu <- as.numeric(as.character(res$score_exclu))
-                 res$P_mendel <- as.numeric(as.character(res$P_mendel))
-
-                 # Order by Mendelian transmission probabilities
-                 res2 <- res[order(res[,4], res[,3], decreasing = T),]
-
-                 delta_P12 <- res2[1,4]-res2[2,4]
-                 delta_P23 <- res2[2,4]-res2[3,4]
-
-                 p_fin <- res2[1,1]
-                 m_fin <- res2[1,2]
-
-                 ped.out <- c(rownames(tmp), NA, NA)
-                 if (is.na(p_fin)&is.na(m_fin)) {
-                   ped.out[2:3] <- c(NA, NA)
-                 } else {
-                   ped.out[2:3] <- c(p_fin,m_fin)
-                 }
-
-                 log.out <- unlist(c(ped.out[1], ped.log[off,2], ped.out[2:3], res2[1, 3:4],
-                                     res2[2,1:4], delta_P12,
-                                     res2[3,1:4], delta_P23))
-
-                 # Order by mismatches
-                 res2 <- res[order(res[,3], -res[,4], decreasing = F),]
-
-
-                 exclu.out <- unlist(c(log.out[1],ped.log[off,2], res2[1,1:3], res2[2,1:3], res2[3,1:3]))
-
-                 a <- list(ped.out, log.out, exclu.out)
-               }
-
-  # Stop the parallel cluster
-  close(pb)
-  stopCluster(cl)
-
-  ped <- as.data.frame(t(as.data.frame(lapply(A, function(X) {t <- X[[1]]}))))
-  colnames(ped) <- c('off', 'sire', 'dam')
-  rownames(ped) <- c(1:nrow(ped))
-
-  # Working on the data
-  ped$off <- as.character(ped$off)
-  ped$sire <- as.character(ped$sire)
-  ped$dam <- as.character(ped$dam)
-
-  # Create a log
-  ped.log <- as.data.frame(t(as.data.frame(lapply(A, function(X) {t <- X[[2]]}))))
-  colnames(ped.log) <- c('off', 'mrk_genotype','sire1', 'dam1', 'miss1', 'mendel1',
-                         'sire2', 'dam2', 'miss2', 'mendel2', 'delta_Pmendel12',
-                         'sire3', 'dam3', 'miss3', 'mendel3', 'delta_Pmendel23')
-  rownames(ped.log) <- c(1:nrow(ped.log))
-
-  ped.log[,] <- sapply(ped.log[,c(1:ncol(ped.log))], as.character)
-  ped.log[,c(2, 5:6, 9:11, 14:16)] <- sapply(ped.log[,c(2, 5:6, 9:11, 14:16)], as.numeric)
-
-  # Create a data frame from results by exclusion
-  ped.exclu <- as.data.frame(t(as.data.frame(lapply(A, function(X) {t <- X[[3]]}))))
-  colnames(ped.exclu) <- c('off', 'mrk_genotype','sire1', 'dam1', 'miss1',
-                           'sire2', 'dam2', 'miss2',
-                           'sire3', 'dam3', 'miss3')
-  rownames(ped.exclu) <- c(1:nrow(ped.exclu))
-  ped.exclu[,c(2,5,8,11)] <- sapply(ped.exclu[,c(2,5,8,11)], as.numeric)
-
-  # Return the output
-  return(list(pedigree = ped, log.mendel = ped.log, log.exclu = ped.exclu, nb.mrk = ncol(offspring)))
-}
-
-# ----------------------------------------------------------------------------------------------------------------
 #' Assignment function to obtain the average Mendelian transmission probabilities using a Fortran library
 #'
 #' This function calculates the average Mendelian transmission probabilities
@@ -453,6 +118,8 @@ assignment <- function(offspring, sire, dam, thresh = ncol(offspring), preselect
 #' @return intermidiate pedigree
 #' @return log file for Mendelian transmission probabilities
 #' @return log file for exclusion
+#' @example data(genotype_APIS)
+#' assignment <- assignmentFortran(off_full, sire_full, dam_full)
 #' @export
 assignmentFortran <- function(offspring, sire, dam, thresh = ncol(offspring), preselect.Parent = F) {
 
@@ -1096,7 +763,7 @@ personalThreshold <- function(APIS.result, method, threshold = NULL) {
   cat('--------------------------------------', sep = '\n')
   cat("Method for personal threshold : ", method, sep = "")
   cat('\n')
-  cat('Threshold : ', threshold)
+  cat('Threshold : ', threshold, sep = "")
   cat('\n')
   assignmentRate <- length(pedigree$sire[which(is.na(pedigree$sire) == F)]) / nrow(pedigree)
   AR <- substr(as.character(100 * assignmentRate), 1, 6)
